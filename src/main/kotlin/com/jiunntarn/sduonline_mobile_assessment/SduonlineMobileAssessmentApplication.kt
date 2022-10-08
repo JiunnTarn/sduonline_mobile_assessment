8961d0429a687c84df2a13dd9f9073d034c64142
JiunnTarn <aencil@yeah.net>
2022-10-08 12:40:34 +0800

    init

---
+++ src/main/kotlin/com/jiunntarn/sduonline_mobile_assessment/SduonlineMobileAssessmentApplication.kt
@@ -0,0 +1,11 @@
+package com.jiunntarn.sduonline_mobile_assessment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SduonlineMobileAssessmentApplication
+
+fun main(args: Array<String>) {
+  runApplication<SduonlineMobileAssessmentApplication>(*args)
+}
---
+++ src/test/kotlin/com/jiunntarn/sduonline_mobile_assessment/SduonlineMobileAssessmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jiunntarn.sduonline_mobile_assessment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SduonlineMobileAssessmentApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
